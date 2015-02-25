59d0b6ec09d563a3ab576c75255f537a27455238
Noriyuki Abe <kenko.py@gmail.com>
2015-02-25 23:39:22 +0900

    Add mods

---
+++ mod/default/mod_default.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+
+from mod import Mod
+import random
+import os
+
+
+class ModDefault(Mod):
+    def __init__(
+        self,
+        filename=None,
+        logger=None
+    ):
+        Mod.__init__(self, logger)
+        text_path = filename or os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            "default.txt"
+        )
+        self.default_texts = [line.strip() for line in open(text_path)]
+
+    def is_fire(self, message, master):
+        return True
+
+    def reses(self, message, master):
+        return [
+            (random.random(),
+             text, "default", dict())
+            for text in self.default_texts
+        ]
