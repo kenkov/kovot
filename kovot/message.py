dd4d9fc7b1fc580fbad655a87d15f017cead10c2
kenkov <kenkovtan@gmail.com>
2018-06-27 01:58:01 +0900

    MessageTransformerクラスを実装

--- kovot/message.py
+++ kovot/message.py
@@ -2,14 +2,22 @@
 # coding:utf-8
 
 
-from collections import namedtuple
-
-
 class Message:
-    def __init__(self, text, id_=0, user=None):
+    def __init__(self, text, id_=None, user=None):
         self.text = text
         self.id_ = id_
         self.user = user
 
+    def __eq__(self, other):
+        return all([self.text == other.text,
+                    self.id_ == other.id_,
+                    self.user == other.user])
 
-User = namedtuple("User", ["name"])
+    def __repr__(self):
+        fmt = "Message(text={x.text}, id_={x.id_}, user={x.user})"
+        return fmt.format(x=self)
+
+
+class MessageTransformer:
+    def transform(self, message):
+        return message
