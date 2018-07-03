4cf6789bb42ae633a83ddced75a930a2a74f3d5c
kenkov <kenkovtan@gmail.com>
2018-07-03 18:24:41 +0900

    Slack Stream の name を screen_name に変更

--- kovot/stream/slack.py
+++ kovot/stream/slack.py
@@ -13,18 +13,19 @@ from slackclient import SlackClient
 
 
 class Slack:
-    def __init__(self, realname):
-        self.realname = realname
-        slack_token = os.environ["SLACK_API_TOKEN"]
-        self.sc = SlackClient(slack_token)
-        self.id2realname = self._get_id2realname()
+    def __init__(self, channel, name, token):
+        self.channel = channel
+        self.name = name
+
+        self.sc = SlackClient(token)
+        self.id2name = self._get_id2name()
         self.messages = deque([])
 
-    def _get_id2realname(self):
-        return {item["id"]: item["real_name"]
+    def _get_id2name(self):
+        return {item["id"]: item["name"]
                 for item
                 in self.sc.api_call("users.list", limit=100)["members"]
-                if "real_name" in item}
+                if "name" in item}
 
     def __iter__(self):
         con = self.sc.rtm_connect()
@@ -43,17 +44,15 @@ class Slack:
             logging.debug(f"slack: {reses}")
             if reses:
                 for item in reses:
-                    realname = self.id2realname.get(item.get("user", None),
-                                                    None)
+                    name = self.id2name.get(item.get("user", None), None)
                     if (item["type"] == "message" and
-                            realname != self.realname):
+                            name != self.name):
                         self.messages.append(item)
             if self.messages:
                 break
             time.sleep(1)
         mess = self.messages.popleft()
-        return Message(id_=0,
-                       text=self._get_text(mess),
+        return Message(text=self._get_text(mess),
                        speaker=Speaker(name=""))
 
     def _get_text(self, message):
@@ -66,8 +65,8 @@ class Slack:
 
     def post(self, response) -> bool:
         self.sc.api_call("chat.postMessage",
-                         channel="#bot_test",
-                         text=response.text.text,
+                         channel=self.channel,
+                         text=response.text,
                          as_user=True  # bot user のアイコン、名前を設定してツイートする
                          )
         return True
