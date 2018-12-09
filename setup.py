af972cd0237a1c3c87451702ea4fbc33b45ceab2
kenkov <kenkovtan@gmail.com>
2018-12-09 22:42:02 +0900

    依存モジュールを追加

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ from distutils.core import setup
 setup(
     name="kovot",
     packages=["kovot", "kovot.stream"],
+    install_requires=[
+        "slackclient>=1.2.1"
+    ],
     version="0.1.0",
     author="kenkov",
     author_email="kenkovtan@gmail.com",
