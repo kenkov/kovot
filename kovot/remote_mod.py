1ab0a34d16aa4bff6f775d3b1dcd0cb5ccb02abf
kenkov <kenkovtan@gmail.com>
2018-12-17 20:32:20 +0900

    RemoteModを実装

---
+++ kovot/remote_mod.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+
+import requests
+import logging
+from kovot.response import Response
+
+
+class RemoteCallerMod:
+    def __init__(self, server, port):
+        self._root_url = "http://{}:{}".format(server, port)
+
+    def get_responses(self, bot, message):
+        url = "{}/api/get_responses".format(self._root_url)
+        logging.debug("{} requests to {}".format(self.__class__.__name__, url))
+        json = {"message": message.dict()}
+        res = requests.post(url=url, json=json)
+        return [Response.from_dict(res_)
+                for res_ in res.json()["responses"]]
