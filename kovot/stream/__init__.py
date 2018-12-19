88655a6706303d89567558e7930afd361df509d8
kenkov <kenkovtan@gmail.com>
2018-12-19 21:00:26 +0900

    Enables to use main classes without subpackage names

---
+++ kovot/stream/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+
+from kovot.stream.stdio import StdIO
+from kovot.stream.slack import Slack
--- test/test_bot.py
+++ test/test_bot.py
@@ -3,9 +3,10 @@
 
 
 import unittest
-from kovot.bot import Bot
-from kovot.response import Response
-from kovot.message import Message
+from kovot import Bot
+from kovot import Response
+from kovot import Message
+from kovot import Speaker
 
 
 class EchoMod:
@@ -17,7 +18,8 @@ class EchoMod:
 
 class BotTest(unittest.TestCase):
     def test_talk(self):
-        msg = Message(text="テスト")
+        msg = Message(text="テスト",
+                      speaker=Speaker(name="話し✋"))
         bot = Bot(mods=[EchoMod()])
 
         res = bot.talk(msg)
