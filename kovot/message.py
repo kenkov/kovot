45e6b939375c0037b856ff35fe598465bfdf2895
kenkov <kenkovtan@gmail.com>
2018-12-25 13:56:16 +0900

    Messageから任意の引数を取るargvを削除

--- kovot/message.py
+++ kovot/message.py
@@ -6,18 +6,15 @@ import kovot.util
 
 
 class Message(ClassInitReplMixin):
-    def __init__(self, text, id_=None, speaker=None,
-                 **argv):
+    def __init__(self, text, id_=None, speaker=None):
         self.text = text
         self.id_ = id_
         self.speaker = speaker
-        self._argv = argv
 
     def dict(self):
         d = {"text": self.text,
              "id_": self.id_,
-             "speaker": self.speaker,
-             **self._argv}
+             "speaker": self.speaker}
         return kovot.util.dict(d)
 
     def __getattr__(self, name):
