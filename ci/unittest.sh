e3b9d7e9050c9dd819b1799e715123b9419d0f97
kenkov <kenkovtan@gmail.com>
2019-03-24 17:42:49 +0900

    Add CI

--- ci/unittest.sh
+++ ci/unittest.sh
@@ -3,4 +3,4 @@ set -eu
 cd `dirname $0`/..
 
 docker image build -t kovot .
-docker container run --entrypoint python3 dialogapi -m unittest discover -v /kovot/test
+docker container run --entrypoint python3 -m unittest discover -v /kovot/test
